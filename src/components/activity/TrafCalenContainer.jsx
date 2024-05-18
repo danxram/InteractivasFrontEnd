a7436f469c061e0a47a4f910f320f2423921cf9b
AlejandroBolanos <alejandroroblesb@hotmail.com>
2024-05-17 19:15:32 -0600

    Add library dependecies and calendar in home

--- src/components/activity/TrafCalenContainer.jsx
+++ src/components/activity/TrafCalenContainer.jsx
@@ -1,29 +1,38 @@
 import "../../index.css";
 import { EventTraffic } from "../ui/EventTraffic";
+import { Backdrop, CircularProgress } from '@mui/material';
+import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
-export function TrafCalenContainer({items}) {
-    return(
+export function TrafCalenContainer({ items }) {
+    return (
 
         <>
-    <section>
-        <div class="flex flex-col sm:flex-row m-7 md:m-10 lg:m-14 gap-10 w-auto ">
-            {/*Esta es la seccion semaforo*/}
-            <div class="flex flex-col gap-3 md:w-2/4 ">
-            <h1 class="text-[#0F345F] font-main font-bold text-3xl tracking-widest pb-3 sm:pb-6 text-center">UPCOMING EVENTS</h1>
-                {items.map(item=> <EventTraffic key={item.id} eventName={item.eventName} description={item.description} borderColor={item.borderColor} />)}
-            </div>
-            {/*Esta es la seccion calendar*/}
-            <div class="flex flex-col gap-3 text-center">
-                <h2 class="text-[#0F345F] font-main font-bold text-xl tracking-normal">CALENDAR</h2>
-                <div>
+            <section>
+                <div className="flex flex-col sm:flex-row m-7 md:m-10 lg:m-14 gap-10 w-auto ">
+                    {/*Esta es la seccion semaforo*/}
+                    <div className="flex flex-col gap-3 md:w-2/4 ">
+                        <h1 className="text-[#0F345F] font-main font-bold text-3xl tracking-widest pb-3 sm:pb-6 text-center">UPCOMING EVENTS</h1>
+                        {items.map(item => <EventTraffic key={item.id} eventName={item.eventName} description={item.description} borderColor={item.borderColor} />)}
+                    </div>
+                    {/*Esta es la seccion calendar*/}
+                    <div className="flex flex-col gap-3  md:w-2/4 ">
+                        <h2 className="text-[#0F345F] font-main font-bold text-xl tracking-normal justify-start">CALENDAR</h2>
+                        <div className="flex justify-center w-full">
+                            
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DateCalendar className="boder-2 border"/>
+                            </LocalizationProvider>
 
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
 
-    </section>
-    
-    
-    </>
+            </section>
+
+
+        </>
     )
 }
