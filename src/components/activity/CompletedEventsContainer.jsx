4188c1a3463a6d29fc8d770de0ffe198e3c2ff81
neg4nin9 <eddyisaac000@gmail.com>
2024-05-25 18:40:58 -0600

    Replaced event containers with EventContainer in CompletedEventsContainer

--- src/components/activity/CompletedEventsContainer.jsx
+++ src/components/activity/CompletedEventsContainer.jsx
@@ -1,20 +1,22 @@
 
 import '../../index.css';
 import { CardEvent } from '../ui/CardEvent';
+import { EventContainer } from "../activity/EventContainer.jsx";
 
-export function CompletedEventsContainer({ items }) {
+export function CompletedEventsContainer() {
+    const events=[
+        {"id": 1, "eventName": "Event", "imageEvent": "https://th.bing.com/th/id/OIP.hSXNqQe1qgQd8-sq8KqkWAHaFj?rs=1&pid=ImgDetMain", description:"Lorem ipsum dolor sit amet consectetur. Pharetra nisl nibh aliquet convallis enim nisi...", type: "Task", date:"22/02/24"},
+        {"id": 2, "eventName": "Event", "imageEvent": "https://th.bing.com/th/id/OIP.hSXNqQe1qgQd8-sq8KqkWAHaFj?rs=1&pid=ImgDetMain", description:"Lorem ipsum dolor sit amet consectetur. Pharetra nisl nibh aliquet convallis enim nisi...", type: "Task", date:"22/02/24"},
+        {"id": 3, "eventName": "Event", "imageEvent": "https://th.bing.com/th/id/OIP.hSXNqQe1qgQd8-sq8KqkWAHaFj?rs=1&pid=ImgDetMain", description:"Lorem ipsum dolor sit amet consectetur. Pharetra nisl nibh aliquet convallis enim nisi...", type: "Task", date:"22/02/24"},
+]
     return (
         <>
             <div className='flex flex-col px-32 '>
                 <h2 className='text-[#274F7E] max-sm:text-4xl sm:text-5xl font-bold py-10  sm:ml-0 sm:mr-auto'>DAILY</h2>
-                <section class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-7 md:m-10 lg:m-14 justify-items-center gap-10 '>
-                    {items.map(item => <CardEvent key={item.id} eventName={item.eventName} imageEvent={item.imageEvent} date={item.date} description={item.description} type={item.type} />)}
-                </section>
+                <EventContainer items={events} />
 
                 <h2 className='text-[#274F7E] max-sm:text-4xl sm:text-5xl font-bold py-10  sm:ml-0 sm:mr-auto'>WEEKLY</h2>
-                <section class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-7 md:m-10 lg:m-14 justify-items-center gap-10 '>
-                    {items.map(item => <CardEvent key={item.id} eventName={item.eventName} imageEvent={item.imageEvent} date={item.date} description={item.description} type={item.type} />)}
-                </section>
+                <EventContainer items={events} />
             </div>
         </>
     )
